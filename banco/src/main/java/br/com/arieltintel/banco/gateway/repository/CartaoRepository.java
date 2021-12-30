bfef6e70022c8fb497dd7f505da1f66d0bf40650
ArielTintel <arielandrade97@gmail.com>
2021-12-30 19:33:48 -0300

    Adicionado Correcao

--- banco/src/main/java/br/com/arieltintel/banco/gateway/repository/CartaoRepository.java
+++ banco/src/main/java/br/com/arieltintel/banco/gateway/repository/CartaoRepository.java
@@ -11,16 +11,16 @@ import java.math.BigDecimal;
 @Repository
 public interface CartaoRepository extends CrudRepository<Cartao, Long>{
 
-    @Query("select count(obj.id) from Cartao obj where obj.codigoSegurancaCartao = ?1 and obj.nroCartao = ?2")
+    @Query("select count(obj.id) from Cartao obj where obj.codigoSegurancaCartao = ?1 and obj.numeroCartao = ?2")
     Integer findCartaoValido(Integer codigoSegurancaCartao, Integer numeroCartao);
 
-    @Query("select count(obj.id) from Cartao obj where obj.codigoSegurancaCartao = ?1 and obj.nroCartao = ?2 and obj.valorCredito >= ?3")
+    @Query("select count(obj.id) from Cartao obj where obj.codigoSegurancaCartao = ?1 and obj.numeroCartao = ?2 and obj.valorCredito >= ?3")
     Integer isSaldoSuficiente(Integer codigoSegurancaCartao, Integer numeroCartao, BigDecimal valorCompra);
 
-    @Query("from Cartao obj where obj.codigoSegurancaCartao = ?1 and obj.nroCartao = ?2")
+    @Query("from Cartao obj where obj.codigoSegurancaCartao = ?1 and obj.numeroCartao = ?2")
     Cartao findCartao(Integer codigoSegurancaCartao, Integer numeroCartao);
 
     @Modifying
-    @Query("update Cartao set valorCredito = (valorCredito - ?3) where codigoSegurancaCartao = ?1 and nroCartao = ?2 ")
-    void atualizarSaldo(Integer codigoSegurancaCartao, Integer nroCartao, BigDecimal valorCompra);
+    @Query("update Cartao set valorCredito = (valorCredito - ?3) where codigoSegurancaCartao = ?1 and numeroCartao = ?2 ")
+    void atualizarSaldo(Integer codigoSegurancaCartao, Integer numeroCartao, BigDecimal valorCompra);
 }
